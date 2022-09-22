77a54f541a79516a2211c800a472ca07e896d009
maddenlockin <maddenlockin@gmail.com>
2022-09-22 14:13:23 -0700

    updated fireEvent syntax for tests - now passing

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
 describe('Color Router', () => {
@@ -11,16 +11,14 @@ describe('Color Router', () => {
     expect(label.innerHTML).toEqual('rgb(192,192,192)');
 
     // Find & click the "Crimson" link to navigate to the next page:
-    const crimson = screen.getByText(/crimson/i);
-    crimson.click();
+    fireEvent.click(screen.getByText(/crimson/i));
 
     // Check that the label updated its values to match the "crimson" color:
     label = screen.getByText(/rgb\(\d+,\d+,\d+\)/i);
     expect(label.innerHTML).toEqual('rgb(220,20,60)');
 
     // Find & click the "Purple" link to navigate to the next page:
-    const purple = screen.getByText(/purple/i);
-    purple.click();
+    fireEvent.click(screen.getByText(/purple/i));
 
     // Check that the label updated its values to match the "purple" color:
     label = screen.getByText(/rgb\(\d+,\d+,\d+\)/i);
